29c8ef7f321d04d2bb16c9c616268922198739a6
Tracy Chen <tracy200e@gmail.com>
2024-06-01 16:50:02 +1000

    Update: put entire component into a container

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -6,64 +6,66 @@ import clsx from "clsx";
 export const Hero: React.FC = () => {
   return (
     <>
-      <div className="flex items-center bg-secondary p-28 my-10 rounded-3xl">
-        <div className="grid gap-y-6">
-          <h1 className="text-6xl font-semibold">
-            Book a trustworthy companion for{" "}
-            <span className="italic">your</span> companion
-          </h1>
-          <h3 className="text-xl">
-            Finding a dog walker should only take a few minutes.
-          </h3>
-          <div className="flex items-center gap-10">
-            <button className="flex items-center justify-center py-1 px-3 bg-primary rounded-3xl text-white text-sm">
-              Book a walk
-              <Image
-                src="../images/heart-handshake-icon.svg"
-                alt="heart handshake icon"
-                className="pl-1"
-                width={28}
-                height={28}
-              />
-            </button>
-            <div className="flex items-center gap-1">
-              <p className="text-primary">Learn more</p>
-              <Image
-                src="../images/arrow-right-circle-icon.svg"
-                alt="right arrow icon"
-                className="pl-1"
-                width={28}
-                height={28}
-              />
+      <div className="container">
+        <div className="flex items-center bg-secondary p-28 my-10 rounded-3xl">
+          <div className="grid gap-y-6">
+            <h1 className="text-6xl font-semibold">
+              Book a trustworthy companion for{" "}
+              <span className="italic">your</span> companion
+            </h1>
+            <h3 className="text-xl">
+              Finding a dog walker should only take a few minutes.
+            </h3>
+            <div className="flex items-center gap-10">
+              <button className="flex items-center justify-center py-1 px-3 bg-primary rounded-3xl text-white text-sm">
+                Book a walk
+                <Image
+                  src="../images/heart-handshake-icon.svg"
+                  alt="heart handshake icon"
+                  className="pl-1"
+                  width={28}
+                  height={28}
+                />
+              </button>
+              <div className="flex items-center gap-1">
+                <p className="text-primary">Learn more</p>
+                <Image
+                  src="../images/arrow-right-circle-icon.svg"
+                  alt="right arrow icon"
+                  className="pl-1"
+                  width={28}
+                  height={28}
+                />
+              </div>
             </div>
           </div>
-        </div>
-        <div className="overflow-visible">
-          <Image
-            src="../images/dog (1).svg"
-            alt="Cute sitting dog"
-            width={900}
-            height={691}
-          />
-        </div>
-      </div>
-      <div className="flex items-center bg-secondary p-28 my-10 rounded-3xl overflow-visible">
-        <div>
-          <Image
-            src="../images/dog (2).svg"
-            alt="Cute standing dog"
-            width={579}
-            height={579}
-          />
-        </div>
-        <div className="grid gap-y-6">
-          <div>
-            <h1 className="text-4xl font-bold">Getting started?</h1>
-            <h1 className="text-4xl font-bold">It's a walk in the park!</h1>
+          <div className="overflow-visible">
+            <Image
+              src="../images/dog (1).svg"
+              alt="Cute sitting dog"
+              width={900}
+              height={691}
+            />
           </div>
-          <h3 className="text-xl">
-            Finding a dog walker should only take a few minutes.
-          </h3>
+        </div>
+        <div className="flex items-center bg-secondary p-28 my-10 rounded-3xl overflow-visible">
+          <div>
+            <Image
+              src="../images/dog (2).svg"
+              alt="Cute standing dog"
+              width={579}
+              height={579}
+            />
+          </div>
+          <div className="grid gap-y-6">
+            <div>
+              <h1 className="text-4xl font-bold">Getting started?</h1>
+              <h1 className="text-4xl font-bold">It's a walk in the park!</h1>
+            </div>
+            <h3 className="text-xl">
+              Finding a dog walker should only take a few minutes.
+            </h3>
+          </div>
         </div>
       </div>
     </>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -4,28 +4,30 @@ import Image from "next/image";
 export const Navbar: React.FC = () => {
   return (
     <>
-      <header className="flex justify-between items-center min-h-2 py-4">
-        <Image
-          src="../images/logo.svg"
-          alt="Pawfect Days logo"
-          className="flex gap-2 items-center"
-          width={264}
-          height={40}
-        />
-        <div className="flex items-center justify-center gap-10">
-          <p className="text-primary">Log in</p>
-          <button className="flex items-center justify-center py-1 px-3 bg-primary rounded-3xl text-white text-sm border-solid border-4 border-orange-400">
-            Walk a dog
-            <Image
-              src="../images/dog-icon.svg"
-              alt="Dog icon"
-              className="pl-1"
-              width={28}
-              height={28}
-            />
-          </button>
-        </div>
-      </header>
+      <div className="container">
+        <header className="flex justify-between items-center min-h-2 py-4">
+          <Image
+            src="../images/logo.svg"
+            alt="Pawfect Days logo"
+            className="flex gap-2 items-center"
+            width={264}
+            height={40}
+          />
+          <div className="flex items-center justify-center gap-10">
+            <p className="text-primary">Log in</p>
+            <button className="flex items-center justify-center py-1 px-3 bg-primary rounded-3xl text-white text-sm border-solid border-4 border-orange-400">
+              Walk a dog
+              <Image
+                src="../images/dog-icon.svg"
+                alt="Dog icon"
+                className="pl-1"
+                width={28}
+                height={28}
+              />
+            </button>
+          </div>
+        </header>
+      </div>
     </>
   );
 };
